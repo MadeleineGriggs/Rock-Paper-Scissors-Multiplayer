caebd04ec12a8125bb3c7250b06498418ecb74d0
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-07-06 19:53:31 -0400

    did some basic styling, now users must create a username before seeing the rooms, then hides the rooms when they join a game. Added function to display the current players in a game by username.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -14,10 +14,10 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 var localID = 0;
+var remotePlayerIndex;
 var name = "";
 var roomID = null;
 var roomUserID = null;
-var inGame = false;
 
 //When the document is ready, create a unique localID for the player. On the user disconnecting,
 // remove the localID.
@@ -38,26 +38,55 @@ $(".choice-btn").on("click", function(event) {
     }
 });
 
+
+function displayRemotePlayerUsername(remUserName) {
+    database.ref("players/" + remUserName).once("value").then(function(snapshot){
+        var remoteUsername = snapshot.val().username;
+        $("#curPlayers").text(name + ", " + remoteUsername);
+    })
+}
+
+
 function updateGame(snapshot) {
     var playerCount = snapshot.numChildren();
     if (playerCount > 0) {
         var players = snapshot.val();
         var keys = Object.keys(players);
-        console.log(keys);
-
+        $("#curPlayers").text(name);
         if (playerCount == 2) {
             // start playing the game
+            localIndex = keys.indexOf(localID)
+            remotePlayerIndex = keys[1-localIndex];
+            console.log(remotePlayerIndex);
+            displayRemotePlayerUsername(remotePlayerIndex)
+
             var waiting = false;
             for (var i in keys) {
                 if (players[keys[i]].move === -1) {
                     waiting = true;
-                    console.log("waiting for player" + i + " move")
+
                 }
             }
             if (!waiting) {
                 localPlayerIndex = keys.indexOf(localID);
+                // Line below evaluates who won. It uses the 'moveVal' of the players choice as a math formula, then 
+                // returns the character at the calculated value. 
+                // The result will always be the result of the local player.  
                 result = "TWLLTWWLT".charAt(players[keys[1-localPlayerIndex]].move * 3 + players[keys[localPlayerIndex]].move);
-                console.log("Match Result: " + result);
+                // Display who one or lost to the players.
+                switch (result) {
+                    case "W" :
+                        $("#game-messages").text("You Won!");
+                    break;
+                    case "T" :
+                        $("#game-messages").text("You Tied with the other player!");
+                    break;
+                    case "L" :
+                        $("#game-messages").text("You Lost!");
+                    default:
+                    break;
+                }
+
                 database.ref("rooms/" + roomID + "/players/" + localID).update({
                     move: -1
                 });
@@ -82,14 +111,15 @@ function updateGame(snapshot) {
             }
         } else {
             // wait for another player
-            console.log("waiting for P2");
+            $("#game-messages").text("waiting for another player to start the game.");
         }
     }
 }
 
 // Lets the player join the room they have selected.
 function joinRoom (roomKey) {
-    console.log("Joining room " + roomKey);
+    $("#room-modal").toggleClass("hidden");
+    $("#game-modal").toggleClass("hidden");
     // Disconnect from current room
     if (roomID !== null) {
         database.ref("rooms/" + roomID + "/players/" + localID).remove();
@@ -105,19 +135,10 @@ function joinRoom (roomKey) {
         move: -1
     });
     roomID = roomKey;
-    inGame = true;
     database.ref("rooms/" + roomID +"/players/" + localID).onDisconnect().remove();
 }
 
-function deleteRoom (roomKey) {
-    console.log("Deleting room " + roomKey);
-    database.ref("rooms/" + roomKey).remove();
 
-}
-
-database.ref("players/").on('value', function(snapshot) {
-    // console.log(snapshot);
-});
 
 //When the database sees that a room has been added, generate the html for the room name and buttons and append to the 'room-wrapper' area.
 database.ref("rooms/").on("value", function(snapshot) {
@@ -125,53 +146,64 @@ database.ref("rooms/").on("value", function(snapshot) {
     // console.log(roomState)
     //Empty the room wrapper when there is a new room.
     $("#rooms-wrapper").empty();
-
+    
     var html = $("<div>").addClass("room-list");
-
+    
     for (var key in roomState) {
         var line = $("<div>");
         line.addClass("room-line")
-            .attr("room-code", key)
-            .text("Title: " + roomState[key].title);
+        .attr("room-code", key)
+        .text("Title: " + roomState[key].title);
         // If there are less than 2 players in a game room, let another player join using
         // the join room button. If there are 2 players the join room button is not available.
         if (snapshot.child(key + "/players/").numChildren() < 2){
             line.append(
                 $("<button>")
-                    .attr("room-code", key)
-                    .addClass("join-room")
-                    .text("Join Room")
-                    .click(function (e) {
-                        joinRoom($(e.target).attr("room-code"));
-                    })
-            )
-        }
-        line.append(
-            $("<button>")
-                .addClass("delete-room")
-                .text("Delete Room")
-                .click(function() {
-                    deleteRoom(key);
+                .attr("room-code", key)
+                .addClass("join-room")
+                .text("Join Room")
+                .click(function (e) {
+                    joinRoom($(e.target).attr("room-code"));
                 })
-        )
-        html.prepend(line);
-    }
-    $("#rooms-wrapper").append(html);
-});
+                )
+            }
+            // When testing I had a delete-room button, so I could easily delete a room. 
+            // I don't currently want that, since any user could delete the room...
+            // line.append(
+                //     $("<button>")
+                //         .addClass("delete-room")
+                //         .text("Delete Room")
+                //         .click(function() {
+                    //             deleteRoom(key);
+                    //         })
+                    // )
+                    html.prepend(line);
+                }
+                $("#rooms-wrapper").append(html);
+            });
+            
+            //Removes the room by pressing the delete room button. Mostly just for ease of testing.
+            // function deleteRoom (roomKey) {
+            //     database.ref("rooms/" + roomKey).remove();
+            
+            // }
 
-// Gets the player's username from the "#username" input and sends to the players section of the database.
-$("#create-player").on("click", function(event) {
-    event.preventDefault();
-
-    name = $("#username").val().trim();
-
-    database.ref('players/' + localID).set({
-        username: name,
-        wins: 0,
-        losses: 0,
-        ties: 0
+            // Gets the player's username from the "#username" input and sends to the players section of the database.
+    $("#create-player").on("click", function(event) {
+        event.preventDefault();
+        
+        name = $("#username").val().trim();
+        
+        database.ref('players/' + localID).set({
+            username: name,
+            wins: 0,
+            losses: 0,
+            ties: 0
+        });
+        $("#username-modal").addClass("hidden");
+        $("#room-modal").toggleClass("hidden");
+        $("#localUsername").text(name);
     });
-});
 
 //Gets the room name from the "#roomTitle" input and sends to the rooms section of the database.
 $("#create-room").on("click", function(event) {
@@ -184,70 +216,3 @@ $("#create-room").on("click", function(event) {
         history: []
     });
 });
-
-$("#start-game").on("click", function(event) {
-    event.preventDefault();
-
-    
-
-    // database.ref().once('value', function(snapshot) {
-    //     if (snapshot.hasChild("game-Room")) {
-
-    //     } else {
-    //         database.ref().set("game-Room");
-    //         database.ref().set("localPlayerTrack");
-    //         database.ref("localPlayerTrack/").set(localPlayer)
-    //     }
-    // })
-
-
-    // database.ref('game-Room/').push();
-
-
-    // var userName = "";
-
-    // database.ref().on('value', function(snapshot) {
-    //     if (snapshot.localPlayer == 0) {
-    //         console.log("localPlayer Value is 0" + snapshot.localPlayer)
-    //         localPlayer = 1;
-    //         localPlayerUpdate = ({
-    //             localPlayerTrack: '1'
-    //         })
-    //         database.ref("localPlayer/").set(localPlayerUpdate)
-
-    //         userName = ({
-    //             username: name
-    //         });
-    //         database.ref('game-Room/p1/').set(userName)
-
-    //     } else if (snapshot.localPlayer == "1") {
-    //         console.log("localPlayer value is: " + snapshot)
-    //         localPlayer = 2;
-    //         localPlayerUpdate = ({
-    //             localPlayer: 2
-    //         })
-    //         database.ref("localPlayer/").set(localPlayerUpdate)
-    //         userName = ({
-    //             username: name
-    //         })
-    //         database.ref('game-Room/p2/').set(userName)
-    //     }
-    // })
-
-});
-
-// database.ref("game-Room/").on("child_added", function(snapshot) {
-// sv = snapshot.val();
-// var player1Location = sv.id;
-// console.log("Checking if I can get player location: " + player1Location)
-// console.log("checking if location matches to playerKey: " + player1Key)
-// })
-
-
-
-// database.ref("game-Room/").on("value", function(snapshot) {
-//     usersWaiting = snapshot.numChildren();
-//     if (usersWaiting == 2) {
-//         alert("Two players Ready to play! Lets start...")
-//     }
-// });
